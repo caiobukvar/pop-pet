eca3fef9afc5e7a754bf0ba5adedb0047c1f38d7
Caio Bukvar <bukvar2009@gmail.com>
2022-01-27 14:57:15 -0300

    instalação react hook form, react router dom, toastify, react dom

--- src/app/routes.js
+++ src/app/routes.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 // import { StoresProvider, useStores } from '../stores';
 
+const Login = React.lazy(async () => import('../containers/Login').then((m) => ({ default: m.Login })));
+const Register = React.lazy(async () => import('../containers/Register').then((m) => ({ default: m.Register })));
 const Home = React.lazy(async () => import('../containers/Home').then((m) => ({ default: m.Home })));
+const Admin = React.lazy(async () => import('../containers/Admin').then((m) => ({ default: m.Admin })));
 
 // function ProtectedRoutes(props) {
 //     const { userStore: { token } } = useStores();
@@ -15,23 +18,18 @@ const Home = React.lazy(async () => import('../containers/Home').then((m) => ({ 
 // }
 
 export function Routes() {
-    return (
-        // <StoresProvider>
-        <Switch>
-            <Route path="/" exact render={() => <Redirect to="/login" />} />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
+  return (
+    // <StoresProvider>
+    <Switch>
+      <Route path="/" exact render={() => <Redirect to="/login" />} />
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
 
-
-            <ProtectedRoutes path="/home">
-                <Route path="/home" component={Home} />
-                <Route path="/home/admin" component={Admin} />
-            </ProtectedRoutes>
-
-
-            <Route path="*" component={Page404} />
-
-        </Switch>
-        // </StoresProvider>
-    );
+      {/* <ProtectedRoutes path="/home"> */}
+      <Route path="/home" component={Home} />
+      <Route path="/home/admin" component={Admin} />
+      {/* </ProtectedRoutes> */}
+    </Switch>
+    // </StoresProvider>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ToastContainer } from 'react-toastify';
+import App from './app/App';
 import './index.scss';
-import Home from './app/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    {/* <StoresProvider> */}
+    {/* <BrowserRouter> */}
+    <App />
+    <ToastContainer />
+    {/* </BrowserRouter> */}
+    {/* </StoresProvider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
