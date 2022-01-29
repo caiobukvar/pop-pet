07ba9dde489047d86b250813507c836df3db16e6
Caio Bukvar <bukvar2009@gmail.com>
2022-01-29 16:50:08 -0300

    adding history push to login

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,22 +2,29 @@ import style from './style.module.scss';
 import MagnifyingGlass from '../../assets/img/magnifying-glass.svg';
 import ShoppingCart from '../../assets/img/shopping-cart/shopping-cart.svg';
 import Login from '../../assets/img/login-button.svg';
+import AddToCart from '../../assets/img/shopping-cart/shopping-cart-add.svg';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export function Home() {
-  const [searchTerm, setSearchTerm] = useState();
+  const [searchTerm, setSearchTerm] = useState('');
+  const history = useHistory();
+
+  function handleLogin() {
+    history.push("/login");
+  }
 
   const products = [
-    { id: 1, name: 'Smooth rubber ball', category: 'Toys', price: 1290, stock: 6 },
-    { id: 2, name: 'Chewing bone', category: 'Toys', price: 500, stock: 3 },
-    { id: 3, name: 'Dog food - Special Dog', category: 'Food', price: 2790, stock: 12 },
-    { id: 4, name: 'Collar', category: 'Utility', price: 2290, stock: 4 },
-    { id: 5, name: 'Bed for dogs', category: 'Utility', price: 3990, stock: 1 },
-    { id: 6, name: 'Bed for cats', category: 'Utility', price: 2990, stock: 0 },
-    { id: 7, name: 'Scratcher for cats', category: 'Toys', price: 3200, stock: 2 },
-    { id: 8, name: 'Catnip', category: 'Utility', price: 1500, stock: 1 },
-    { id: 9, name: 'Clover rubber ball', category: 'Toys', price: 1000, stock: 0 },
+    { id: 1, name: 'Smooth rubber ball', category: 'Toys', price: 12.90, stock: 6 },
+    { id: 2, name: 'Chewing bone', category: 'Toys', price: 5.00, stock: 3 },
+    { id: 3, name: 'Dog food - Special Dog', category: 'Food', price: 27.90, stock: 12 },
+    { id: 4, name: 'Collar', category: 'Utility', price: 22.90, stock: 4 },
+    { id: 5, name: 'Bed for dogs', category: 'Utility', price: 39.90, stock: 1 },
+    { id: 6, name: 'Bed for cats', category: 'Utility', price: 29.90, stock: 0 },
+    { id: 7, name: 'Scratcher for cats', category: 'Toys', price: 32.00, stock: 2 },
+    { id: 8, name: 'Catnip', category: 'Utility', price: 15.00, stock: 1 },
+    { id: 9, name: 'Clover rubber ball', category: 'Toys', price: 10.00, stock: 0 },
   ]
 
   function inputFilter(val) {
@@ -27,7 +34,7 @@ export function Home() {
     if (val.category.toLowerCase().includes(searchTerm.toLowerCase())) {
       return val;
     }
-    if (val.price.toLowerCase().includes(searchTerm.toLowerCase())) {
+    if (val.price === searchTerm) {
       return val;
     }
 
@@ -56,7 +63,7 @@ export function Home() {
           <button className={style.cart} type="button">
             <img src={ShoppingCart} alt="" />
           </button>
-          <button className={style.login} type="button">
+          <button className={style.login} type="button" onClick={handleLogin}>
             <p>Login</p>
             <img src={Login} alt="" />
           </button>
@@ -67,11 +74,29 @@ export function Home() {
       </div>
       <div className={style.content}>
         <div className={style.outdoor} />
-        <h2>Conheça nossos produtos</h2>
+        <h2>Take a look at our products:</h2>
         <div className={style.products}>
-          {products.filter(inputFilter).map(product => (
+          {products.filter(inputFilter).map((product) => (
             <div key={product.id} className={style['card-container']}>
-              <span>{product.name}</span>
+              <h3>{product.name}</h3>
+              <div className={style['card-image']}>
+                <img src={product.image} alt="" />
+              </div>
+              <div>
+                {product.price.toLocaleString('en-us', { style: 'currency', currency: 'USD' })}
+              </div>
+              <div className={style['card-bottom']}>
+                {product.stock === 0 ?
+                  <div className={style['add-to-cart-disabled']}>
+                    Unavailable
+                  </div>
+                  :
+                  <button className={style['add-to-cart']}>
+                    Add to cart
+                    <img src={AddToCart} alt="cart icon" />
+                  </button>
+                }
+              </div>
             </div>
           )
           )}
