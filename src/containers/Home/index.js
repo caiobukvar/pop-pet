56fea480c7c70df30a049a562ed075db155c61bc
Caio Bukvar <bukvar2009@gmail.com>
2022-01-31 19:45:39 -0300

    get products

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -8,35 +8,27 @@ import notify from '../../utils/notify';
 export function Home() {
   const [searchTerm, setSearchTerm] = useState('');
   const [productModalOpen, setProductModalOpen] = useState(false);
+  const [productArray, setProductArray] = useState([]);
 
   useEffect(() => {
-    try {
-      const result = api.get('/products');
+    async function getProducts() {
+      try {
+        const result = await api.get('/products');
+        const { data } = result;
+        setProductArray(data);
 
-      if (result.status !== 200) {
-        throw 'Couldn&apos;t reach data!';
-      }
-
-      console.log(result);
-    } catch (error) {
-      const { request } = error;
-      if (request) {
-        notify('error', request.response);
+      } catch (error) {
+        const { request } = error;
+        if (request) {
+          notify('error', request.response);
+        }
       }
     }
+    getProducts();
   }, []);
 
-  const products = [
-    { id: 1, name: 'Smooth rubber ball', category: 'Toys', price: 12.90, stock: 6 },
-    { id: 2, name: 'Chewing bone', category: 'Toys', price: 5.00, stock: 3 },
-    { id: 3, name: 'Dog food - Special Dog', category: 'Food', price: 27.90, stock: 12 },
-    { id: 4, name: 'Collar', category: 'Utility', price: 22.90, stock: 4 },
-    { id: 5, name: 'Bed for dogs', category: 'Utility', price: 39.90, stock: 1 },
-    { id: 6, name: 'Bed for cats', category: 'Utility', price: 29.90, stock: 1 },
-    { id: 7, name: 'Scratcher for cats', category: 'Toys', price: 32.00, stock: 2 },
-    { id: 8, name: 'Catnip', category: 'Utility', price: 15.00, stock: 0 },
-    { id: 9, name: 'Clover rubber ball', category: 'Toys', price: 10.00, stock: 0 },
-  ]
+  const products = productArray;
+
 
   function inputFilter(val) {
     if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
