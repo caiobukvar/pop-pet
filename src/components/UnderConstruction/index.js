b1180c31c6d1f05c4aefb6b506240d5e78255081
Caio Bukvar <bukvar2009@gmail.com>
2022-02-02 10:58:49 -0300

    component created

---
+++ src/components/UnderConstruction/index.js
@@ -0,0 +1,28 @@
+import style from './style.module.scss';
+import Construction from '../../assets/img/construction.svg'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+
+export default function UnderConstruction() {
+  const history = useHistory();
+
+  function handleGoBack() {
+    history.goBack();
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.content}>
+        <h2>Whoops!</h2>
+        <p>This page is still under construction!</p>
+        <img src={Construction}
+          alt="under cosntruction"
+          className={style.construction}
+        />
+        <button className={style.home} onClick={handleGoBack}>
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/containers/Checkout/index.js
@@ -0,0 +1,12 @@
+import style from './style.module.scss';
+import UnderConstruction from '../../components/UnderConstruction';
+
+
+export function Checkout() {
+
+  return (
+    <div className={style.container}>
+      <UnderConstruction />
+    </div>
+  );
+}
