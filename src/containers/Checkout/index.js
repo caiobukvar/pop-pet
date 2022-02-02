d251510641a4f2fd9614e4e2b77e6b63a6aa0bcb
Caio Bukvar <bukvar2009@gmail.com>
2022-02-02 10:37:57 -0300

    fix button

--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -14,7 +14,7 @@ export function Checkout() {
     <div className={style.container}>
       <div className={style.content}>
         <h2>Whoops!</h2>
-        <p>This page is still under construction</p>
+        <p>This page is still under construction!</p>
         <img src={Construction}
           alt="under cosntruction"
           className={style.construction}
